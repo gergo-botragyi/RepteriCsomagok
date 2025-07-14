15a870e8bd6f18f469eb9625947614893ff93aaf
gergo-botragyi <71039555+gergo-botragyi@users.noreply.github.com>
2025-07-14 14:59:40 +0200

    Delete out/build/cpp directory

--- out/build/cpp/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/suli/Airport
-# Build directory: D:/suli/Airport/out/build/cpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
